6a9f503c2df62e2025d0adfb45fe2d5111c6c1fe
nishupriya08 <105414426+nishupriya08@users.noreply.github.com>
2026-06-16 10:44:37 +0000

    Conversions of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "50ab"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33ab" => NaN
+// true => 1
+
+let isLoggedIn = true;
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn);
+
+//1 => true
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = Number (someNumber)
+console.log(stringNumber);
+
