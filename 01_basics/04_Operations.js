bced38908bbd96092c72d9537b81a9c74f6f468e
nishupriya08 <105414426+nishupriya08@users.noreply.github.com>
2026-06-16 10:58:50 +0000

    Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,35 @@
+//**************************Operation***********************
+
+let value = 3
+let negValue = -(value)
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Nishu"
+let str2 = " Priya"
+console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
