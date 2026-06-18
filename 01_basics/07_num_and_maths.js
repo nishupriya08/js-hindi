ee505724da6414ac6586eadf2fa5c554b4e3d374
nishupriya08 <105414426+nishupriya08@users.noreply.github.com>
2026-06-18 11:22:43 +0000

    math.num

---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,34 @@
+const score = 100
+
+const balanace = new Number(100)
+//console.log(balanace);
+
+
+//console.log(balanace.toString().length);
+//console.log(balanace.toFixed(2));
+
+const otherNumber = 23.8765;
+//console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++ 
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(5.7));
+// console.log(Math.floor(5.4));
+// console.log(Math.min(3,5,6,7,1));
+// console.log(Math.max(5,9, 4,6 ,87));
+
+console.log(Math.random());
+console.log((Math.random() *10)+1);
+
+const min = 10
+const  max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1))+ min)
